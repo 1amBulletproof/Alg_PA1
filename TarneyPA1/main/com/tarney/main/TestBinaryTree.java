ccf60916a25c8f00a01e911b6a8b80d77e65fe55
1amBulletproof <brandon.tarney@gmail.com>
2016-02-15 14:37:44 -0500

    moved main method to its own package

---
+++ TarneyPA1/main/com/tarney/main/TestBinaryTree.java
@@ -0,0 +1,22 @@
+/** Original code was provided by "Introduction to Java Programming" 
+ * 8th Edition by Y. Daniel Liang, pages 858 - 869
+ *
+ *  This code is used for educational purposes only
+ *  in 605.421 Foundations of Algorithms. 
+ */
+
+
+package com.tarney.main;
+
+import com.tarney.tree.BinaryTree;
+
+public class TestBinaryTree {
+	
+  public static void main(String[] args) {
+
+	//TODO change this class to the submitted main class 
+	//TODO input for the program: file? Command line? Gui? hard-coded?
+	//TODO needs to run *at least* height(), postOrderTraversal(), getNumberOfLeaves(), getNumberOfNonLeaves()
+
+  }
+}
